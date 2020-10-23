d2d930f1c7f79340dcf3cf56a3cdcd2ee37ae3b2
Sweta5201 <sweta65959@gmail.com>
2020-10-23 21:48:00 +0530

    Added : method in main file for the testcase

--- src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
@@ -1,7 +1,9 @@
 package com.capgemini.tdd.invoiceservice;
 
 public class InvoiceGenerator {
+	private static final int RATE_PER_DAY = 250;
+
 	public int calculateRate(int days) {
-		return 0;
+		return RATE_PER_DAY * days;
 	}
 }
