c3026bf293070fe27a256ba5d21681783142642d
Your Name <you@example.com>
2020-10-24 10:25:09 +0530

    [shashi] Refactored . Method for calculation of multiple rides is added

--- src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
@@ -12,4 +12,11 @@ public class InvoiceGenerator {
 		return totalFare;
 	}
 
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
 }
--- src/test/java/com/capgemini/tdd/invoiceservice/InvoiceGeneratorTest.java
+++ src/test/java/com/capgemini/tdd/invoiceservice/InvoiceGeneratorTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 public class InvoiceGeneratorTest {
 
 	@Test
-	public void givenDistanceAndTime_shouldReturnTotalFare() {
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
@@ -15,11 +15,19 @@ public class InvoiceGeneratorTest {
 	}
 
 	@Test
-	public void givenLessDistanceAndTime_shouldReturnTotalMinFare() {
+	public void givenLessDistanceAndTime_ShouldReturnTotalMinFare() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, fare, 0.0);
 	}
 }
