a6c1dd8af506f723f05479fb95d78c67ee37c101
Your Name <you@example.com>
2020-10-24 09:28:11 +0530

    [shashi] Added . calculateFare method is made for first testCase

--- src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/capgemini/tdd/invoiceservice/InvoiceGenerator.java
@@ -1,8 +1,10 @@
 package com.capgemini.tdd.invoiceservice;
 
 public class InvoiceGenerator {
+	public final double MINIMUM_COST_PER_KILOMETER = 10.0;
+	public final int COST_PER_TIME = 1;
 
 	public double calculateFare(double distance, int time) {
-		return 0;
+		return (distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME);
 	}
 }
